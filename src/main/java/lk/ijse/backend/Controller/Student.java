b84ffdf2319fcf3d65355be3335b971c3786bb28
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-07-15 20:40:08 +0530

    update files

--- src/main/java/lk/ijse/backend/Controller/Student.java
+++ src/main/java/lk/ijse/backend/Controller/Student.java
@@ -1,20 +1,30 @@
 package lk.ijse.backend.Controller;
 
 
+//import jakarta.json.bind.Jsonb;
+//import jakarta.json.bind.JsonbBuilder;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.backend.DTO.StudentDTO;
-
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.*;
 
-@WebServlet(urlPatterns = "/student")
-public class student extends HttpServlet {
+@WebServlet(urlPatterns = "/stu")
+public class Student extends HttpServlet {
     private Connection con;
 
     @Override
