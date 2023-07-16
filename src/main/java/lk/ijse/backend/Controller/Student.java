3510fc48ee135b462551a6017056f3123e3c1e11
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-07-16 10:54:50 +0530

    update files

--- src/main/java/lk/ijse/backend/Controller/Student.java
+++ src/main/java/lk/ijse/backend/Controller/Student.java
@@ -4,7 +4,6 @@ import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import lk.ijse.backend.DTO.StudentDTO;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
